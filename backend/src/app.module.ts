2e384ddc6699e86955c216163a2d7dedda59bc32
Antonio Luiz Carvalho Turano Filho <turano@usp.br>
2025-05-16 00:12:40 -0300

    feat(users): hash user password with bcrypt on create and update

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from 'prisma/prisma.module';
 import { UsersModule } from './users/users.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [PrismaModule, UsersModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    PrismaModule,
+    UsersModule,
+  ],
   controllers: [],
   providers: [],
 })
--- backend/src/users/service/users.service.ts
+++ backend/src/users/service/users.service.ts
@@ -2,14 +2,33 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import { PrismaService } from 'prisma/prisma.service';
 import { CreateUserDto } from '../dto/create-user.dto';
 import { UpdateUserDto } from '../dto/update-user.dto';
+import * as bcrypt from 'bcrypt';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class UsersService {
-  constructor(private readonly prisma: PrismaService) {}
+  private readonly saltRounds: number;
+
+  constructor(
+    private readonly prisma: PrismaService,
+    private configService: ConfigService,
+  ) {
+    this.saltRounds = parseInt(
+      this.configService.get<string>('SALT_ROUNDS') ?? '10',
+      10,
+    );
+  }
 
   async create(createUserDto: CreateUserDto) {
+    const hashedPassword = await bcrypt.hash(
+      createUserDto.password,
+      this.saltRounds,
+    );
     return this.prisma.user.create({
-      data: createUserDto,
+      data: {
+        ...createUserDto,
+        password: hashedPassword,
+      },
     });
   }
 
@@ -31,6 +50,13 @@ export class UsersService {
 
   async update(id: string, updateUserDto: UpdateUserDto) {
     await this.findOne(id);
+
+    if (updateUserDto.password) {
+      updateUserDto.password = await bcrypt.hash(
+        updateUserDto.password,
+        this.saltRounds,
+      );
+    }
 
     return this.prisma.user.update({
       where: { id },
