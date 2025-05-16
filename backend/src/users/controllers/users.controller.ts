acade3e481772c7a649fa019ea83f47ec4021e64
Antonio Luiz Carvalho Turano Filho <turano@usp.br>
2025-05-16 01:32:59 -0300

    chore: rename controller and service folders to plural

--- backend/src/users/controllers/users.controller.ts
+++ backend/src/users/controllers/users.controller.ts
@@ -9,7 +9,7 @@ import {
   HttpCode,
   HttpStatus,
 } from '@nestjs/common';
-import { UsersService } from '../service/users.service';
+import { UsersService } from '../services/users.service';
 import { CreateUserDto } from '../dto/create-user.dto';
 import { UpdateUserDto } from '../dto/update-user.dto';
 import { UserResponseDto } from '../dto/user-response.dto';
--- backend/src/users/users.module.ts
+++ backend/src/users/users.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
-import { UsersController } from './controller/users.controller';
-import { UsersService } from './service/users.service';
+import { UsersController } from './controllers/users.controller';
+import { UsersService } from './services/users.service';
 
 @Module({
   controllers: [UsersController],
